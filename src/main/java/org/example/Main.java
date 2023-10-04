1d04f5550003e3881f05eb999dee29e5faea9c8c
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-10-04 17:05:08 +0200

    Add lines to Main.java for order commands

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.ShopService.Order;
+import org.example.ShopService.OrderListRepo;
 import org.example.ShopService.Product;
 import org.example.ShopService.ProductRepo;
 
@@ -8,6 +10,7 @@ import java.math.BigDecimal;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Product/ProductRepo");
         ProductRepo newProductRepo = new ProductRepo();
         Product product1 = new Product("11111", "Product 1", new BigDecimal("19.99"), 5);
         Product product2 = new Product("22222", "Product 2", new BigDecimal("29.99"), 8);
@@ -17,5 +20,18 @@ public class Main {
         System.out.println("newProductRepo after adding: " + newProductRepo);
         newProductRepo.removeProduct(product2);
         System.out.println("newProductRepo after removing product2: " + newProductRepo);
+
+        System.out.println("Order/OrderListRepo");
+        OrderListRepo newOrderListRepo = new OrderListRepo();
+        Order order1 = new Order("jamieLannister", "29-9-2023", new BigDecimal("19.99"), "King's Landing", new ProductRepo[]{product1, product2});
+        Order order2 = new Order("ladyStoneheart1337", "21-9-2023", new BigDecimal("19.99"), "remote work address", new ProductRepo[]{product1, product2});
+        System.out.println("newOrderListRepo before adding: " + newOrderListRepo);
+        newOrderListRepo.addOrder(order1);
+        newOrderListRepo.addOrder(order2);
+        System.out.println("newOrderListRepo after adding: " + newOrderListRepo);
+        newOrderListRepo.removeOrders(order2);
+        System.out.println("newOrderListRepo after removing order2: " + newOrderListRepo);
+
+
     }
 }
