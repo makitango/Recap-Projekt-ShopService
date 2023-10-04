9abcbfc9820c8577df65135554f392a5da21edc7
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-10-04 11:37:54 +0200

    Create empty test file

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-    //GIVEN
-    //WHEN
-    //THEN
+    //Coding: ProductRepo
+    @Test
+    public void name() {
+    }
 }
