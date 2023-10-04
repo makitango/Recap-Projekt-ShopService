fa6d0c9061f0b9b973a68b64da23bba47dd70799
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-10-04 17:04:34 +0200

    Fix Order

--- src/main/java/org/example/ShopService/Order.java
+++ src/main/java/org/example/ShopService/Order.java
@@ -3,5 +3,5 @@ package org.example.ShopService;
 import java.math.BigDecimal;
 
 // Schritt 1: Erstelle einen Record Order mit den benötigten Attributen.
-public record Order(String userName, String orderDate, BigDecimal totalPrice, String deliveryAddress) {
+public record Order(String userName, String orderDate, BigDecimal totalPrice, String deliveryAddress, ProductRepo products) {
 }
