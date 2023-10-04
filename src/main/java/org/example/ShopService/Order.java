364712c58848f4f0541cb2cad4b1b9c75487c0f8
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-10-04 16:23:17 +0200

    Create Order record

---
+++ src/main/java/org/example/ShopService/Order.java
@@ -0,0 +1,7 @@
+package org.example.ShopService;
+
+import java.math.BigDecimal;
+
+// Schritt 1: Erstelle einen Record Order mit den benötigten Attributen.
+public record Order(String userName, String orderDate, BigDecimal totalPrice, String deliveryAddress) {
+}
